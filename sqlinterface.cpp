042cd76db417a574d3ddc3e539f3844625316c28
chester <survivor_wqw133@163.com>
2025-12-13 20:51:58 +0800

    第一次提交

---
+++ sqlinterface.cpp
@@ -0,0 +1,3 @@
+#include "sqlinterface.h"
+
+SQLInterface::SQLInterface() {}
---
+++ sqlinterface.h
@@ -0,0 +1,10 @@
+#ifndef SQLINTERFACE_H
+#define SQLINTERFACE_H
+
+class SQLInterface
+{
+public:
+    SQLInterface();
+};
+
+#endif // SQLINTERFACE_H
